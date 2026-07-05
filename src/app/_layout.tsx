b490fde620a0c2d8cf0d0f93595c1d4dab09cd01
Minkxx <monsurcodes@gmail.com>
2026-07-06 00:34:47 +0530

    feat: redirect to login page if not authenticated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,25 @@
+import { authClient } from "@/api/auth-client";
+import { LoginScreen } from "@/features/auth/LoginScreen";
 import { Stack } from "expo-router";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
+  const { data: session, isPending } = authClient.useSession();
+  if (isPending) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#4f46e5" />
+      </View>
+    );
+  }
+
+  if (!session) {
+    return <LoginScreen />;
+  }
+
   return <Stack />;
 }
+
+const styles = StyleSheet.create({
+  center: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
